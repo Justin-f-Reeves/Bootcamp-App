42d3b319ab27a563888169e1c07e1348d96f8176
Justin Reeves <mail.to.justin@gmail.com>
2022-11-23 15:33:18 -0500

    save minor changes before clearing env from Git history

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config()
 }
 
-console.log(process.env.SECRET)
-console.log(process.env.API_KEY)
-
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -22,20 +19,13 @@ const userRoutes = require('./routes/users');
 const { timeSince } = require('./utils/humanTime');
 
 
-
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
-// for now we'll just hardcode this URL in (default port for Mongoose)
-// eventually this is going to have some logic in here that says - 
-// "use our local development database" or, if this is in production, then
-// "use the production database"
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
-// just some logic to check if there's an error with the connection to the database,
-// and if there isn't then to show that it's connected.
 
 const app = express();
 
@@ -48,7 +38,7 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret',
+    secret: process.env.secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
